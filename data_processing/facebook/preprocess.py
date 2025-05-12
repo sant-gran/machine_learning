9bbc33a2ef5d36fb4c7567968ee9ce7487045461
Santiago Enrique Granados Forero <santiagog@macbookpro.home>
2025-05-12 18:11:32 -0400

    Node Future Engineering (not built-in)

--- data_processing/facebook/preprocess.py
+++ data_processing/facebook/preprocess.py
@@ -1,13 +1,38 @@
-import gzip
+import torch
+import networkx as nx
+from torch_geometric.utils import from_networkx
 
+def extract_node_features(graph: nx.Graph):
+    features = {}
 
-# Read the first few lines of the compressed file to inspect the format
-file_path = "/Users/santiagog/Desktop/facebook_data/facebook_combined.txt.gz"
+    # Degree
+    degree_dict = dict(graph.degree())
+    nx.set_node_attributes(graph, degree_dict, "degree")
 
-lines =[]
-with gzip.open(file_path, 'rt') as f:
-    for _ in range (10):
-        lines.append(f.readline().strip())
+    # PageRank
+    pagerank_dict = nx.pagerank(graph, alpha=0.85)
+    nx.set_node_attributes(graph, pagerank_dict, "pagerank")
 
-print(lines)
+    # Clustering Coefficient
+    clustering_dict = nx.clustering(graph)
+    nx.set_node_attributes(graph, clustering_dict, "clustering")
 
+    return graph
+
+def convert_to_pyg(graph: nx.Graph):
+    graph = extract_node_features(graph)
+
+    # Convert node features into tensors
+    for node in graph:
+        attrs = graph.nodes[node]
+        graph.nodes[node]['x'] = torch.tensor([
+            attrs["degree"],
+            attrs["pagerank"],
+            attrs["clustering"]
+        ], dtype= torch.float)
+
+    # Convert to PyG format
+        pyg_daa = from_networkx(graph)
+        pyg_daa.x = torch.stack([graph.nodes[n]['x'] for n in graph.nodes])
+
+        return pyg_daa
